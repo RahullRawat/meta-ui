94a62c223d5eca424c71c9b579114f5f2bfd6327
Rahul Rawat <rahulsinghrawat760@gmail.com>
2022-04-02 17:58:30 +0530

    feat : added Toast component

---
+++ components/toast/toast.js
@@ -0,0 +1,18 @@
+const btnToast = document.querySelector(".btn-toast");
+const toast = document.querySelector(".toast");
+const toastClose = document.querySelector(".toast-close");
+
+const showToast = () => {
+	console.log("show");
+	toast.classList.add("show-toast");
+	setTimeout(() => {
+		toast.classList.remove("show-toast");
+	}, 2000);
+};
+
+const hideToast = () => {
+	toast.classList.remove("show-toast");
+};
+
+toastClose.addEventListener("click", hideToast);
+btnToast.addEventListener("click", showToast);
